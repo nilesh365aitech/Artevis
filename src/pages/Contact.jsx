2fac7ec8aa3e48a2b7f1e624e92e52440470cac5
coderHarsh121212 <codesource287@gmail.com>
2024-06-18 17:48:08 +0530

    resolve

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import wmapImg from "../assets/wmap.png";
+import wmapImg from "../assets/ContactMap.png";
 import Footer from "../components/FooterSection";
 import ContactImage from "../assets/contact.png";
 
@@ -11,87 +11,89 @@ export default function Widget() {
   return (
     <>
       <div
-        className=" text-white font-sans bg-cover bg-center"
-        style={{ backgroundImage: `url(${ContactImage})` }}
+        className="bg-black text-white h-[100vh] md:h-[80vh] pb-10 mb-4 relative "
+        style={{
+          backgroundImage: `url(${ContactImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        <div className="container pt-4 pr-4 mx-auto px-4  py-2">
-          <Navbar />
-          <div className="flex flex-wrap py-14">
-            <div className="w-full lg:w-2/3">
-              <h1 className="text-4xl ml-14 font-bold mb-4"><span className="text-red-500" >Heading</span> goes here</h1>
-              <p className="text-lg ml-14 mb-8 mr-3">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                indust's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
+        <Navbar />
+        <div></div>
+        <div className="sm:w-[50%] w-[70%] py-5 px-3  bg-white  min-h-fit text-black mt-20 flex flex-col gap-2 pt-9 rounded-sm border-r-8 border-red-600">
+          <p className="ml-4 sm:ml-14 font-bold" style={{ color: "#FF6347" }}>
+            Connect with us
+          </p>
+          <h1 className="text-4xl font-bold  ml-4 sm:ml-14 ">
+            Have questions or need expert guidance?
+          </h1>
+          <p className="ml-4 sm:ml-14 pr-10">
+            Reach out to us now at info@artevisconsulting.com today, and let's
+            chart the path to your success together.
+          </p>
+        </div>
+        <div className=" text--800 p-5  absolute right-16 top-20 border-2 w-[30%] h-fit rounded">
+          <h2 className="text-lg font-semibold mb-4 ">
+            Generate the master plan to fast track your product to launch
+          </h2>
+          <form>
+            <div className="mb-3">
+              <input
+                type="text"
+                placeholder="Add your full name"
+                className="w-full p-2 border border-zinc-300 rounded-md placeholder-black text-sm text-gray-500"
+              />
             </div>
-
-            <div
-              className="w-full lg:w-1/3"
-              style={{ backdropFilter: "blur(5px)", backgroundColor: "#58595B" }}
+            <div className="mb-3">
+              <input
+                type="email"
+                placeholder="youremailaddress@gmail.com"
+                className="w-full p-2 border  placeholder-black border-zinc-300 rounded-md text-sm text-gray-500"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="tel"
+                placeholder="Add your contact number"
+                className="w-full p-2 border  placeholder-black border-zinc-300 rounded-md text-sm text-gray-500"
+              />
+            </div>
+            <div className="mb-3">
+              <textarea
+                placeholder="Tell us about your project"
+                className="w-full p-3 border  placeholder-black border-zinc-300 rounded-md text-sm text-gray-500"
+                rows="4"
+              ></textarea>
+            </div>
+            <button
+              onClick={handleSubmit}
+              className="w-full bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-md text-sm"
             >
-              <div className=" text--800 p-6 ">
-                <h2 className="text-xl font-semibold mb-4">
-                  Generate the master plan to fast track your product to launch
-                </h2>
-                <form>
-                  <div className="mb-4">
-                    <input
-                      type="text"
-                      placeholder="Add your full name"
-                      className="w-full p-3 border border-zinc-300 rounded-md placeholder-black"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <input
-                      type="email"
-                      placeholder="youremailaddress@gmail.com"
-                      className="w-full p-3 border  placeholder-black border-zinc-300 rounded-md"
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <input
-                      type="tel"
-                      placeholder="Add your contact number"
-                      className="w-full p-3 border  placeholder-black border-zinc-300 rounded-md"
-                    />
-                  </div>
-                  <div className="mb-6">
-                    <textarea
-                      placeholder="Tell us about your project"
-                      className="w-full p-3 border  placeholder-black border-zinc-300 rounded-md"
-                      rows="4"
-                    ></textarea>
-                  </div>
-                  <button
-                    onClick={handleSubmit}
-                    className="w-full bg-red-500 hover:bg-red-600 text-white font-bold py-3 px-4 rounded-md"
-                  >
-                    Get a custom quote
-                  </button>
-                </form>
-              </div>
-            </div>
-          </div>
+              Get a custom quote
+            </button>
+          </form>
         </div>
       </div>
-      <div className="max-w-4xl mx-auto ml-4 md:ml-20 px-4 py-28">
+      {/* <div className="max-w-4xl mx-auto ml-4 md:ml-20 px-4 py-28">
         <h2 className="text-3xl font-semibold">Core Service Offerings</h2>
         <p className="mt-4 text-zinc-600 :text-zinc-300">
-        At our consulting firm, our core service offerings are designed to empower businesses to thrive in today's dynamic landscape. From strategic planning and transformational leadership to market entry strategies and operational optimization, we provide tailored solutions that drive sustainable growth and success. With a focus on innovation, collaboration, and client-centricity, we partner with organizations to unlock their full potential, achieve their goals, and stay ahead of the competition.
+          At our consulting firm, our core service offerings are designed to
+          empower businesses to thrive in today's dynamic landscape. From
+          strategic planning and transformational leadership to market entry
+          strategies and operational optimization, we provide tailored solutions
+          that drive sustainable growth and success. With a focus on innovation,
+          collaboration, and client-centricity, we partner with organizations to
+          unlock their full potential, achieve their goals, and stay ahead of
+          the competition.
         </p>
-      </div>
+      </div> */}
 
-      <img className="w-full md:h-auto h-60" src={wmapImg} alt="Map" />
+      <img className="w-4/5 md:h-auto h-60 mx-auto" src={wmapImg} alt="Map" />
 
       <div className="max-w-4xl mx-auto ml-9 md:ml-36 py-24">
-        <h2 className="text-3xl font-bold mb-4">Work with us</h2>
+        <h2 className="text-3xl font-bold mb-4 text-[#1E227F]">Work with us</h2>
         <p className="text-zinc-700 mb-6">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+        Join our team of experts and contribute to the real world.
         </p>
         <a
           href="#"
