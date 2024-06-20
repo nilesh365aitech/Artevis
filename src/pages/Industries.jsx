fc4e7c9e6bc4ce24eb54e3e9fb92a9d76a709159
nilesh <nilesh@365aitech.com>
2024-06-20 17:00:20 +0530

    responsiveness

--- src/pages/Industries.jsx
+++ src/pages/Industries.jsx
@@ -5,9 +5,60 @@ import MapImage from "../assets/fp.png";
 import RecentUpdates from "../section/RecentUpdates";
 import ContactUs from "../components/ContactUs";
 import Footer from "../components/FooterSection";
+import CaseStudy1 from "../assets/Case Study 1.png";
+import CaseStudy2 from "../assets/Case Study 2.png";
+import CaseStudy3 from "../assets/Case Study 3.png";
+import CaseStudy4 from "../assets/Case Study 4.png";
+import country1 from "../assets/country1.png";
+import country2 from "../assets/country2.png";
+import country3 from "../assets/country3.png";
+import country4 from "../assets/country4.png";
 
 const Industries = () => {
   const [selectedRegion, setSelectedRegion] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const items = [
+    {
+      bgurl: CaseStudy1,
+      title: "Greenfield Ocean Race Project Achieves X% Feasibility Milestone",
+      description:
+        "Formulated a feasibility model for a sports and event management company for launch of a greenfield ocean race initiative in Saudi Arabia...",
+    },
+    {
+      bgurl: CaseStudy2,
+      title:
+        "From Concept to Capital: How an Innovative F&B E-Commerce and FinTech Startup Raised $xxx Million",
+      description:
+        "Crafted an Investor Memorandum and Financial Model to propel Fundraising Efforts for an Innovative F&B E-Commerce and FinTech Startup in KSA",
+    },
+    {
+      bgurl: CaseStudy3,
+      title:
+        "Strategic Investment Spark: From IPO Dreams to Multibillion-Dollar Reality, a Fund's Multifold Exit Triumph",
+      description:
+        "Assisting a New York Hedge Fund in analyzing and investing in an E-Betting Platform, leading to a multibillion-dollar IPO and resulting in a multifold exit for the fund",
+    },
+    {
+      bgurl: CaseStudy4,
+      title:
+        "Strategic Investment Spark: From IPO Dreams to Multibillion-Dollar Reality, a Fund's Multifold Exit Triumph",
+      description:
+        "Assisting a New York Hedge Fund in analyzing and investing in an E-Betting Platform, leading to a multibillion-dollar IPO and resulting in a multifold exit for the fund",
+    },
+  ];
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? items.length - 3 : prevIndex - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === items.length - 3 ? 0 : prevIndex + 1
+    );
+  };
 
   const regionContent = {
     "North America": (
@@ -299,39 +350,39 @@ const Industries = () => {
         </div>
       </div>
       <div className="flex flex-wrap justify-evenly items-center px-4 py-14 bg-white gap-5 sm:gap-0">
-          <div className="text-center mb-6 sm:mb-0">
-            <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
-              {experience}+
-            </p>
-            <p className="text-xs ml-3 sm:ml-0 sm:text-lg md:text-xl lg:text-2xl">
-              Years of Experience
-            </p>
-          </div>
-          <div className="text-center mb-6 sm:mb-0">
-            <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
-              {clients}+
-            </p>
-            <p className="text-xs ml-2 sm:ml-0 sm:text-lg md:text-xl lg:text-2xl">
-              Clients Empowered
-            </p>
-          </div>
-          <div className="text-center mb-6 sm:mb-0">
-            <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
-              {countries}+
-            </p>
-            <p className="text-xs mr-4 sm:mr-0 sm:text-lg md:text-xl lg:text-2xl">
-              Countries Served
-            </p>
-          </div>
-          <div className="text-center mb-6 sm:mb-0">
-            <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
-              ${clientAUM}Bn+
-            </p>
-            <p className="text-xs mr-4 sm:mr-0 sm:text-lg md:text-xl lg:text-2xl">
-              Client AUM
-            </p>
-          </div>
+        <div className="text-center mb-6 sm:mb-0">
+          <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
+            {experience}+
+          </p>
+          <p className="text-xs ml-3 sm:ml-0 sm:text-lg md:text-xl lg:text-2xl">
+            Years of Experience
+          </p>
         </div>
+        <div className="text-center mb-6 sm:mb-0">
+          <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
+            {clients}+
+          </p>
+          <p className="text-xs ml-2 sm:ml-0 sm:text-lg md:text-xl lg:text-2xl">
+            Clients Empowered
+          </p>
+        </div>
+        <div className="text-center mb-6 sm:mb-0">
+          <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
+            {countries}+
+          </p>
+          <p className="text-xs mr-4 sm:mr-0 sm:text-lg md:text-xl lg:text-2xl">
+            Countries Served
+          </p>
+        </div>
+        <div className="text-center mb-6 sm:mb-0">
+          <p className="text-sm mb-3 font-bold text-red-600 sm:text-xl md:text-2xl lg:text-3xl">
+            ${clientAUM}Bn+
+          </p>
+          <p className="text-xs mr-4 sm:mr-0 sm:text-lg md:text-xl lg:text-2xl">
+            Client AUM
+          </p>
+        </div>
+      </div>
       <div className="relative overflow-hidden bg-zinc-100 :bg-zinc-900">
         <img src={MapImage} alt="World Map" className="w-full h-auto" />
         <div
@@ -339,7 +390,7 @@ const Industries = () => {
           onClick={() => setSelectedRegion("North America")}
         >
           <img
-            src="https://placehold.co/40x40"
+            src={country1}
             alt="North America"
             className="w-6 h-6 rounded-full"
           />
@@ -349,11 +400,7 @@ const Industries = () => {
           className="absolute left-[48%] top-[27%] flex items-center space-x-2 bg-white :bg-zinc-800 shadow-lg rounded-full py-1 px-3 cursor-pointer"
           onClick={() => setSelectedRegion("Europe")}
         >
-          <img
-            src="https://placehold.co/40x40"
-            alt="Europe"
-            className="w-6 h-6 rounded-full"
-          />
+          <img src={country2} alt="Europe" className="w-6 h-6 rounded-full" />
           <span className="text-black :text-white">Europe</span>
         </div>
         <div
@@ -361,7 +408,7 @@ const Industries = () => {
           onClick={() => setSelectedRegion("Saudi Arabia")}
         >
           <img
-            src="https://placehold.co/40x40"
+            src={country3}
             alt="Saudi Arabia"
             className="w-6 h-6 rounded-full"
           />
@@ -371,11 +418,7 @@ const Industries = () => {
           className="absolute left-[71%] top-[41%] flex items-center space-x-2 bg-white :bg-zinc-800 shadow-lg rounded-full py-1 px-3 cursor-pointer"
           onClick={() => setSelectedRegion("GCC")}
         >
-          <img
-            src="https://placehold.co/40x40"
-            alt="GCC"
-            className="w-6 h-6 rounded-full"
-          />
+          <img src={country4} alt="GCC" className="w-6 h-6 rounded-full" />
           <span className="text-black :text-white">GCC</span>
         </div>
       </div>
@@ -394,11 +437,70 @@ const Industries = () => {
           <div className="mt-10">{regionContent[selectedRegion]}</div>
         </div>
       )}
-      
+
+<div className="slider-container  relative mt-20 max-w-7xl mx-auto pb-16">
+        <div className="slider overflow-hidden">
+          <div
+            className="slider-content flex transition-transform duration-500 ease-in-out"
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {items.map((item, index) => (
+              <div
+                key={index}
+                className="w-full lg:w-1/3 flex-shrink-0 mb-20 px-2"
+              >
+                <div className="h-72 sm:h-96">
+                  <div
+                    className="bg-cover bg-center h-full"
+                    style={{ backgroundImage: `url(${item.bgurl})` }}
+                  ></div>
+                </div>
+                <div className="flex flex-col gap-3 mt-3">
+                  <p className="text-lg sm:text-xl font-semibold text-gray-900">
+                    {item.title}
+                  </p>
+                  <p className="text-sm sm:text-base font-normal text-gray-700">
+                    {item.description}
+                  </p>
+                </div>
+                <div className="flex justify-between items-center mt-2">
+                  <button className="rounded px-3 py-2 bg-red-200 text-xs text-red-600">
+                    Start Ups
+                  </button>
+                  <a href="#" className="text-red-600 text-xs font-semibold">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="absolute top-[-3rem] right-0 left-0 flex  items-center  justify-between px-3">
+          <h1 className="text-xl sm:text-2xl font-semibold text-blue-900">
+            Success Stories
+          </h1>
+          <div className="flex gap-3 ">
+            <button
+              className="bg-gray-400 px-3 py-1 rounded-full text-white"
+              onClick={prevSlide}
+            >
+              &#10094;
+            </button>
+            <button
+              className="bg-gray-400 px-3 py-1 rounded-full text-white"
+              onClick={nextSlide}
+            >
+              &#10095;
+            </button>
+          </div>
+        </div>
+       
+      </div>
+
       <div className=" mt-6 md:ml-20 ">
         <RecentUpdates />
         <div className="container mx-auto px-4">
-        <ContactUs />
+          <ContactUs />
         </div>
       </div>
       <Footer />
