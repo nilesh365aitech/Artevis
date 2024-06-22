cbb4bfed916ecf1ad2a906d83dc68ef7c9c2cfc0
coderHarsh121212 <codesource287@gmail.com>
2024-06-22 18:48:48 +0530

    final commit

--- src/components/SucessStories.jsx
+++ src/components/SucessStories.jsx
@@ -4,15 +4,18 @@ import CaseStudy2 from "../assets/Case Study 2.png";
 import CaseStudy3 from "../assets/Case Study 3.png";
 import CaseStudy4 from "../assets/Case Study 4.png";
 import "./Slider.css";
+import { useNavigate } from "react-router-dom";
 
 const SucessStories = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const navigate = useNavigate();
   const items = [
     {
       bgurl: CaseStudy1,
       title: "Greenfield Ocean Race Project Achieves X% Feasibility Milestone",
       description:
         "Formulated a feasibility model for a sports and event management company for launch of a greenfield ocean race initiative in Saudi Arabia...",
+      navigate: "/insights/casestudy3",
     },
     {
       bgurl: CaseStudy2,
@@ -20,6 +23,7 @@ const SucessStories = () => {
         "From Concept to Capital: How an Innovative F&B E-Commerce and FinTech Startup Raised $xxx Million",
       description:
         "Crafted an Investor Memorandum and Financial Model to propel Fundraising Efforts for an Innovative F&B E-Commerce and FinTech Startup in KSA",
+      navigate: "/insights/casestudieed",
     },
     {
       bgurl: CaseStudy3,
@@ -27,18 +31,21 @@ const SucessStories = () => {
         "Strategic Investment Spark: From IPO Dreams to Multibillion-Dollar Reality, a Fund's Multifold Exit Triumph",
       description:
         "Assisting a New York Hedge Fund in analyzing and investing in an E-Betting Platform, leading to a multibillion-dollar IPO and resulting in a multifold exit for the fund",
+      navigate: "/insights/casestudies",
     },
     {
       bgurl: CaseStudy4,
       title: "Feasibility Analysis for a one of its kind Ocean Race Event",
       description:
         "A prominent event and sports management company sought to explore the viability of initiating a greenfield ocean race in Saudi Arabia, requiring a detailed feasibility study and adaptable financial model to make critical planning and operating decisions",
+      navigate: "/insights/casestudy5",
     },
     {
       bgurl: CaseStudy4,
       title: "Strategic Transformation for a National Real Estate Firm",
       description:
         "Desiring a company transformation, the client, a real estate firm, sought a comprehensive strategy revamp across multiple sectors to enhance innovation, customer experience, and profitability",
+      navigate: "/insights/casestudy4",
     },
     {
       bgurl: CaseStudy4,
@@ -46,6 +53,7 @@ const SucessStories = () => {
         "Fundraising Support and Investor Kit Creation for F&B Fintech Startup",
       description:
         "An extensive evaluation of the F&B and FinTech sectors was conducted to determine the Total Addressable Market (TAM) in KSA,revealing the startup’s growth potential",
+      navigate: "/insights/casestudy6",
     },
   ];
 
@@ -93,9 +101,12 @@ const SucessStories = () => {
                   <button className="rounded px-3 py-2 bg-red-200 text-xs text-red-600">
                     Start Ups
                   </button>
-                  <a href="#" className="text-red-600 text-xs font-semibold">
+                  <p
+                    className="text-red-600 text-xs font-semibold cursor-pointer"
+                    onClick={() => navigate(item.navigate)}
+                  >
                     Read More
-                  </a>
+                  </p>
                 </div>
               </div>
             ))}
@@ -152,9 +163,9 @@ const SucessStories = () => {
                   <button className="rounded px-3 py-2 bg-red-200 text-xs text-red-600">
                     Start Ups
                   </button>
-                  <a href="#" className="text-red-600 text-xs font-semibold">
+                  <p className="text-red-600 text-xs font-semibold cursor-pointer" onClick={() => navigate(item.navigate)}>
                     Read More
-                  </a>
+                  </p>
                 </div>
               </div>
             ))}
