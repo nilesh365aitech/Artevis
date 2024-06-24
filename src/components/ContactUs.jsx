bf5895128773477aee221e60e55bef0758880f8f
coderHarsh121212 <codesource287@gmail.com>
2024-06-24 15:20:50 +0530

    laptop

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -25,29 +25,29 @@ export default function ContactUs() {
           <input
             type="text"
             placeholder="Name"
-            className="w-full p-3  -zinc-200 rounded shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
+            className="w-full p-3  -zinc-200  shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
           />
           <input
             type="text"
             placeholder="Phone number"
-            className="w-full p-3  -zinc-200 rounded shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
+            className="w-full p-3  -zinc-200  shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
           />
           <input
             type="email"
             placeholder="Email"
-            className="w-full p-3  -zinc-200 rounded shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
+            className="w-full p-3  -zinc-200  shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
           />
           <input
             type="text"
             placeholder="Location"
-            className="w-full p-3  -zinc-200 rounded shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
+            className="w-full p-3  -zinc-200  shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
           />
           <textarea
             placeholder="Tell us about your needs"
-            className="w-full p-3  -zinc-200 rounded shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
+            className="w-full p-3  -zinc-200  shadow-sm focus:outline-none focus:ring-2 ring-[#F0F3F8] placeholder-blue-900 bg-gray-100"
             rows="4"
           ></textarea>
-          <div className="w-full p-3 bg-blue-900 text-white rounded-md flex items-center justify-between px-6">
+          <div className="w-full p-3 bg-blue-900 text-white -md flex items-center justify-between px-6">
             <button type="submit" className="">
               Submit
             </button>
